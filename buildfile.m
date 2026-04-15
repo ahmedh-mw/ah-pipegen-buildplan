275785a41ae9b2ef8afe1d7d82caac4410d7e757
Ahmed Hassan <ahmedh@mathworks.com>
2026-04-15 14:40:28 -0400

    reorder

--- buildfile.m
+++ buildfile.m
@@ -5,19 +5,17 @@ import matlab.buildtool.tasks.*;
 plan = buildplan();
 
 plan("ci:madv") = ModelAdvisorTask.forEachModel();
-plan("ci:web") = SimulinkWebViewTask.forEachModel();
-% plan("ci:ded") = SimulinkDesignVerifierTask.forEachModel();
-
 plan("ci:codegen") = SimulinkCoderTask.forEachModel();
+plan("ci:codegen").Dependencies = "ci:web";
 
-plan("ci:slci") = SimulinkCodeInspectorTask.forEachModel();
-plan("ci:slci").Dependencies = "ci:codegen";
+plan("ci:web") = SimulinkWebViewTask.forEachModel();
 
+% plan("ci:slci") = SimulinkCodeInspectorTask.forEachModel();
+% plan("ci:slci").Dependencies = "ci:codegen";
+% plan("ci:ded") = SimulinkDesignVerifierTask.forEachModel();
 % plan("ci:mil") = SimulinkTestTask.
-
 % plan("ci:mergeTest") = MergeSimulinkTestResultsTask.
 % plan("ci:mergeTest").Dependencies = "ci:mil";
-% 
 % plan("ci:mtMetrics") = SimulinkDashboardTask.forEachModel();
 % plan("ci:mtMetrics").Dependencies = "ci:mil";
 end
