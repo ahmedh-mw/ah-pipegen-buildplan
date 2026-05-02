fd46ae5c728b0c6e2a0fed03d756467af39dafd2
Ahmed Hassan <ahmedh@mathworks.com>
2026-05-02 01:04:04 -0400

    Swap architecture settings in generate_jenkins_pipeline function

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -15,8 +15,8 @@ function generate_jenkins_pipeline(pipelineGenDirectory)
     % op.BuildPlanFilePath = "buildfile_ci.m";
     % op.BuildPlanFilePath = "buildfile_original.m";
     % op.Architecture = pipegen.pipeline.Architecture.SerialJobs;
-    % op.Architecture = pipegen.pipeline.Architecture.RootDagJobs;
-    op.Architecture = pipegen.pipeline.Architecture.IndependentModelJobs;
+    op.Architecture = pipegen.pipeline.Architecture.RootDagJobs;
+    % op.Architecture = pipegen.pipeline.Architecture.IndependentModelJobs;
     op.Platform = pipegen.pipeline.Platform.Jenkins;
     % op.TemplatePath = "templates/generic-job.yml";
     op.RunnerTags = "selfhosted_win_agents";
