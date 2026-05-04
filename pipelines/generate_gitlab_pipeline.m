854f4ca664347ef054710c6b4460193bf8d8c6b4
Ahmed Hassan <ahmedh@mathworks.com>
2026-05-04 10:38:30 -0400

    Update architecture option in Jenkins pipeline generation to use RootDagJobs

--- pipelines/generate_gitlab_pipeline.m
+++ pipelines/generate_gitlab_pipeline.m
@@ -15,8 +15,8 @@ function generate_jenkins_pipeline(pipelineGenDirectory)
     % op.BuildPlanFilePath = "buildfile_ci.m";
     % op.BuildPlanFilePath = "buildfile_original.m";
     % op.Architecture = pipegen.pipeline.Architecture.SerialJobs;
-    % op.Architecture = pipegen.pipeline.Architecture.RootDagJobs;
-    op.Architecture = pipegen.pipeline.Architecture.SerialJobsGroupPerTask;
+    op.Architecture = pipegen.pipeline.Architecture.RootDagJobs;
+    % op.Architecture = pipegen.pipeline.Architecture.SerialJobsGroupPerTask;
     % op.Architecture = pipegen.pipeline.Architecture.IndependentModelJobs;
     op.Platform = pipegen.pipeline.Platform.GitLab;
     % op.TemplatePath = "templates/generic-job.yml";
